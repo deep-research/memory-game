67afe29e570e7ce39aaa18ca499cb40ffdd0d757
Victor <deep-research>
2018-02-04 17:10:56 -0600

    Navbar and Jumbotron created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Jumbotron />
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,14 @@
 import React, {Component} from "react";
+import "./Navbar.css";
 
 class Navbar extends Component {
     render() {
         return (
-          <div>
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-            <a className="navbar-brand" href="/">Top navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">Link</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="/">Disabled</a>
-                </li>
-              </ul>
-              <form className="form-inline mt-2 mt-md-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                <button classNameName="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top d-flex justify-content-around py-3">
+            <a className="navbar-brand" href="/">Memory Game</a>
+            <div className="navbar-brand">Click an image to begin!</div >
+            <div className="navbar-brand">Score: 0 | Top Score: 0</div >
           </nav>
-        </div>
         );
     }
 }
