3f35b441e8764fb880b88dac77c4ec9ab3acb830
Victor <deep-research>
2018-02-05 13:42:51 -0600

    File structure improved

--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import './App.css';
 import Jumbotron from "./components/Jumbotron";
 import Images from "./components/Images";
 import Footer from "./components/Footer";
+import shuffle from 'shuffle-array';
+import images from "./images.json";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Jumbotron />
-        <Images />
-        <Footer />
-      </div>
-    );
-  }
+    state = {
+        score: 0,
+        clickedImages: [],
+        topScore: 0,
+        message: "Click an image to begin!",
+        images: shuffle(images)
+    };
+
+    imgClick = (id) => {
+        if (this.state.clickedImages.includes(id)) {
+            if (this.state.score > this.state.topScore) {
+                this.setState({
+                    topScore: this.state.score
+                })
+            }
+            this.setState({
+                score: 0,
+                clickedImages: [],
+                message: "You guessed incorrectly!",
+                images: shuffle(images)
+            })
+        } else {
+            this.setState({
+                score: this.state.score + 1,
+                clickedImages: [...this.state.clickedImages, id],
+                message: "You guessed correctly!",
+                images: shuffle(images)
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar state={this.state} />
+                <Jumbotron />
+                <Images state={this.state} imgClick={this.imgClick} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,48 +1,11 @@
 import React, {Component} from "react";
 import "./Images.css";
-import shuffle from 'shuffle-array';
 import images from "../../images.json"
-import Navbar from "../Navbar";
 
 class Images extends Component {
-    state = {
-        score: 0,
-        clickedImages: [],
-        topScore: 0,
-        message: "Click an image to begin!",
-        images: shuffle(images)
-    };
-
-    imgClick = (id) => {
-        if (this.state.clickedImages.includes(id)) {
-            if (this.state.score > this.state.topScore) {
-                this.setState({
-                    topScore: this.state.score
-                })
-            }
-            this.setState({
-                score: 0,
-                clickedImages: [],
-                message: "You guessed incorrectly!",
-                images: shuffle(images)
-            })
-        } else {
-            this.setState({
-                score: this.state.score + 1,
-                clickedImages: [...this.state.clickedImages, id],
-                message: "You guessed correctly!",
-                images: shuffle(images)
-            })
-        }
-    }
-    
     render() {
         return (
             <div>
-                <Navbar
-                    score={this.state.score}
-                    topScore={this.state.topScore}
-                    message={this.state.message} />
                 <div className="container">
                     <div className="col">
                     {images.map(image => (
@@ -54,7 +17,7 @@ class Images extends Component {
                             alt={image.name}
                             occupation={image.occupation}
                             location={image.location}
-                            onClick={() => this.imgClick(image.id)}>
+                            onClick={() => this.props.imgClick(image.id)}>
                         </img>
                     ))}
                     </div>
