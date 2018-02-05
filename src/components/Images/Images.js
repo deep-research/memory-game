e70f061597e72eef81b1e88951779bc7e65c942e
Victor <deep-research>
2018-02-05 01:24:33 -0600

    game working in the console

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -7,40 +7,67 @@ shuffle(images)
 
 class Images extends Component {
     state = {
-        clicks: 0
+        score: 0,
+        clickedImages: [],
+        topScore: 0
     };
 
-    imgClick = (i) => {
-        this.setState({
-            clicks: this.state.clicks + 1
-        }, () => {
-        console.log(this.state)
-        })
+    imgClick = (id) => {
+        if (this.state.clickedImages.includes(id)) {
+            if (this.state.score > this.state.topScore) {
+                this.setState({
+                    topScore: this.state.score
+                })
+            }
+            this.setState({
+                score: 0,
+                clickedImages: []
+            }, () => {
+            console.log(this.state.score)
+            console.log(this.state.clickedImages)
+            console.log(this.state.topScore)
+            console.log("You guessed incorrectly!")
+            })
+        } else {
+            this.setState({
+                score: this.state.score + 1,
+                clickedImages: [...this.state.clickedImages, id]
+            }, () => {
+            console.log(this.state.score)
+            console.log(this.state.clickedImages)
+            console.log(this.state.topScore)
+            shuffle(images)
+            this.forceUpdate()
+            console.log("You guessed correctly!")
+            })
+        }
     }
 
     componentDidMount() {
-        console.log(this.state)
+        console.log(this.state.score)
+        console.log(this.state.clickedImages)
+        console.log(this.state.topScore)
     }
 
     render() {
         return (
             <div className="container">
-            <div className="col">
-              {images.map(image => (
-                <img
-                    src={image.image}
-                    key={image.id}
-                    id={image.id}
-                    name={image.name}
-                    alt={image.name}
-                    occupation={image.occupation}
-                    location={image.location}
-                    onClick={() => this.imgClick(image.id)}>
-                </img>
-              ))}
+                <div className="col">
+                {images.map(image => (
+                    <img
+                        src={image.image}
+                        key={image.id}
+                        id={image.id}
+                        name={image.name}
+                        alt={image.name}
+                        occupation={image.occupation}
+                        location={image.location}
+                        onClick={() => this.imgClick(image.id)}>
+                    </img>
+                ))}
+                </div>
+                <br/><br/>
             </div>
-            <br/><br/>
-          </div>
         );
     }
 }
